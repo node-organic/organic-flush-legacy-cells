190d1948a448da654a68baa5ad9642e456b962f4
outbounder <obiwonn@gmail.com>
2018-11-24 15:32:09 +0200

    fix cli(npx) usage

--- bin/organic-flush-legacy-cells.js
+++ bin/organic-flush-legacy-cells.js
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
-
 const path = require('path')
-const exec = require('../lib/exec')
-let hostCWD = process.cwd()
+
 process.chdir(path.resolve(__dirname, '../'))
-if (!process.argv[3]) {
-  exec('npx angel install ' + process.argv[2])
-} else {
-  let templatePath = process.argv[3]
-  if (templatePath.indexOf('/') !== 0) {
-    templatePath = path.resolve(hostCWD, templatePath)
-  }
-  exec('npx angel install ' + process.argv[2] + ' ' + templatePath)
-}
+const Angel = require('organic-angel')
+let angel = new Angel()
+require('angelscripts-install-as-daemon')(angel)
+angel.do('install ' + process.argv[2])
