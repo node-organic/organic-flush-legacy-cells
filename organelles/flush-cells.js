579a7463f15484d1ee9733e2ac0d6c4d2b09e11b
outbounder <obiwonn@gmail.com>
2018-11-25 17:44:01 +0200

    watch deployments/enabled
    + delete deploymentJSON from deployments/running
    + update to latest install as daemon scripts

--- organelles/flush-cells.js
+++ organelles/flush-cells.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const semverDiff = require('semver-diff')
 const StartedCells = require('organic-alchemy-started-cells-info')
 
@@ -11,23 +12,41 @@ module.exports = class {
   }
   onCellMitosisComplete (c, next) {
     let cellInfo = c.cellInfo
-    cellInfo.mitosisPath = c.path
-    this.startedCells.forEach((legacy_cell) => {
+    cellInfo.deploymentEnabledPath = c.path
+    this.startedCells.forEach(async (legacy_cell) => {
+      let legacyCellConditions = legacy_cell.mitosis.apoptosis.versionConditions
       if (legacy_cell.name === cellInfo.name &&
-        is_version_legacy(legacy_cell.mitosis.apoptosis.versionConditions, legacy_cell.version, cellInfo.version)) {
+        this.is_version_legacy(legacyCellConditions, legacy_cell.version, cellInfo.version)) {
         console.info('flushing', legacy_cell)
         this.startedCells.remove(legacy_cell)
-        if (legacy_cell.mitosisPath) {
-          fs.unlink(legacy_cell.mitosisPath, (err) => {
-            if (err) console.info(err, legacy_cell.mitosisPath)
-          })
+        if (legacy_cell.deploymentEnabledPath) {
+          await this.safeDelete(legacy_cell.deploymentEnabledPath)
+          if (legacy_cell.mitosis.zygote) {
+            await this.safeDelete(this.getDeploymentRunningPath(legacy_cell))
+          }
         }
       }
     })
     this.startedCells.add(cellInfo)
   }
-}
-const is_version_legacy = function (versionConditions, existing_version, new_version) {
-  let diffValue = semverDiff(existing_version, new_version)
-  return versionConditions.indexOf(diffValue) !== -1
+  is_version_legacy (versionConditions, existing_version, new_version) {
+    let diffValue = semverDiff(existing_version, new_version)
+    return versionConditions.indexOf(diffValue) !== -1
+  }
+  safeDelete (filepath) {
+    return new Promise((resolve, reject) => {
+      fs.unlink(filepath, (err) => {
+        if (err) console.info(err)
+        resolve()
+      })
+    })
+  }
+  getDeploymentRunningPath (cellInfo) {
+    return path.join(this.dna.runningDeploymensLocation,
+      [
+        cellInfo.name,
+        cellInfo.version,
+        cellInfo.mitosis.mode
+      ].join('-') + '.json')
+  }
 }
